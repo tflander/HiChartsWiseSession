b89fc63305302e22e44257a899a905eb8088ad82
Flanders, Todd <Todd.Flanders@cengage.com>
2016-01-25 12:48:56 -0500

    color palette by job type

--- spike.js
+++ spike.js
@@ -35,6 +35,45 @@ function jobExecutorsForBatch(batch) {
             },[]);
 }
 
+function jobTypesForBatch(batch) {
+    
+    return batches.batches[0].jobSteps.map(function(step) {
+                                           return step.jobType;
+                                           })
+    .sort()
+    .reduce(function(a,b){
+            if (a.slice(-1)[0] !== b) a.push(b);
+            return a;
+            },[]);
+}
+
+function colorPalletByJobType(batch) {
+    var indexcolors = [
+        "#000000", "#FFFF00", "#1CE6FF", "#FF34FF", "#FF4A46", "#008941", "#006FA6", "#A30059",
+        "#FFDBE5", "#7A4900", "#0000A6", "#63FFAC", "#B79762", "#004D43", "#8FB0FF", "#997D87",
+        "#5A0007", "#809693", "#FEFFE6", "#1B4400", "#4FC601", "#3B5DFF", "#4A3B53", "#FF2F80",
+        "#61615A", "#BA0900", "#6B7900", "#00C2A0", "#FFAA92", "#FF90C9", "#B903AA", "#D16100",
+        "#DDEFFF", "#000035", "#7B4F4B", "#A1C299", "#300018", "#0AA6D8", "#013349", "#00846F",
+        "#372101", "#FFB500", "#C2FFED", "#A079BF", "#CC0744", "#C0B9B2", "#C2FF99", "#001E09",
+        "#00489C", "#6F0062", "#0CBD66", "#EEC3FF", "#456D75", "#B77B68", "#7A87A1", "#788D66",
+        "#885578", "#FAD09F", "#FF8A9A", "#D157A0", "#BEC459", "#456648", "#0086ED", "#886F4C",
+        "#34362D", "#B4A8BD", "#00A6AA", "#452C2C", "#636375", "#A3C8C9", "#FF913F", "#938A81",
+        "#575329", "#00FECF", "#B05B6F", "#8CD0FF", "#3B9700", "#04F757", "#C8A1A1", "#1E6E00",
+        "#7900D7", "#A77500", "#6367A9", "#A05837", "#6B002C", "#772600", "#D790FF", "#9B9700",
+        "#549E79", "#FFF69F", "#201625", "#72418F", "#BC23FF", "#99ADC0", "#3A2465", "#922329",
+                                                "#5B4534", "#FDE8DC", "#404E55", "#0089A3", "#CB7E98", "#A4E804", "#324E72", "#6A3A4C"];
+    
+    var jobTypes = jobTypesForBatch(batch);
+    
+    var pallet = {Idle: "white"};
+    
+    for(i=0; i<jobTypes.length; ++i) {
+        pallet[jobTypes[i]] = indexcolors[i];
+    }
+    return pallet;
+}
+
+
 function sortAndInsertIdleJobSteps(jobStepsForExecutor) {
     
     jobStepsForExecutor.sort(function(stepA, stepB) {
@@ -50,7 +89,8 @@ function sortAndInsertIdleJobSteps(jobStepsForExecutor) {
                            elapsed: secondsToElapsed((nextJobStart - previousJobEnd)/1000),
                            startTs: previousJobEnd,
                            endTs: nextJobStart,
-                           jobId: 'Idle'
+                           jobId: 'Idle',
+                           jobType: 'Idle'
                            });
         }
     }
@@ -81,7 +121,7 @@ function fixJobId(jobId) {
     return jobId.substr(parts[0].length+1);
 }
 
-function chartExecutor(batch, cssSelector, executorName) {
+function chartExecutor(batch, palette, cssSelector, executorName) {
     var jobStepsForExecutor = batch.jobSteps.filter(function(step) {
         return step.host == executorName;
     });
@@ -99,13 +139,12 @@ function chartExecutor(batch, cssSelector, executorName) {
     jobStepsForExecutor = sortAndInsertIdleJobSteps(jobStepsForExecutor);
     var series = [];
     for(i=0; i<jobStepsForExecutor.length; ++i) {
+        var jobStep = jobStepsForExecutor[i];
         var job = {
-        name: jobStepsForExecutor[i].jobId,
-        data: [elapsedToSeconds(jobStepsForExecutor[i].elapsed)]
+        name: jobStep.jobId,
+        data: [elapsedToSeconds(jobStep.elapsed)],
+        color: palette[jobStep.jobType]
         };
-        if(job.name == 'Idle') {
-            job.color = 'white';
-        }
         series.push(job);
     }
     
@@ -159,18 +198,12 @@ function loadChartForRandori() {
     
     var batch = batches.batches[0];
     var jobExecutors = jobExecutorsForBatch(batch);
-    
-    for(i=0; i<jobExecutors.length; ++i) {
-        chartId = "chart" + i;
-        $("body").append('<div id="' + chartId + '" class="chart"></div>');
-        // chartExecutor(batch, '#' + chartId, jobExecutors[i]);
-    }
+    var pallete = colorPalletByJobType(batch);
     
     for(chartNo=0; chartNo<jobExecutors.length; ++chartNo) {
         chartId = "chart" + chartNo;
-        chartExecutor(batch, '#' + chartId, jobExecutors[chartNo]);
+        $("body").append('<div id="' + chartId + '" class="chart"></div>');
+        chartExecutor(batch, pallete, '#' + chartId, jobExecutors[chartNo]);
         console.log(chartId);
-        console.log(chartNo);
-        
     }
 }
